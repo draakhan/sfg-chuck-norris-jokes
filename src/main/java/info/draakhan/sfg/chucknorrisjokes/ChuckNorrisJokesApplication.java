86c16efc1fecbbd4cb6360b1afea49e2e8eb1f6b
Bartlomiej Szymanski <B.Szymanski@kainos.com>
2019-04-04 17:49:17 +0200

    Tabs >>>>> spaces !!!

--- src/main/java/info/draakhan/sfg/chucknorrisjokes/ChuckNorrisJokesApplication.java
+++ src/main/java/info/draakhan/sfg/chucknorrisjokes/ChuckNorrisJokesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChuckNorrisJokesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChuckNorrisJokesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisJokesApplication.class, args);
+    }
 
 }
