3725d72322006be51a3217bf5326bc47f9eaf639
Bartlomiej Szymanski <B.Szymanski@kainos.com>
2019-04-24 10:41:10 +0200

    Added a configuration with bean providing quotes

---
+++ src/main/java/info/draakhan/sfg/chucknorrisjokes/config/ChuckConfiguration.java
@@ -0,0 +1,14 @@
+package info.draakhan.sfg.chucknorrisjokes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Configuration
+public class ChuckConfiguration {
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
